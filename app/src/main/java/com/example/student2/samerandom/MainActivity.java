e32d517c54d369cee1995396d906b1640fa8cb32
rokymiel <rokymiel@gmail.com>
2017-12-25 19:50:32 +0300

    Accel

--- app/src/main/java/com/example/student2/samerandom/MainActivity.java
+++ app/src/main/java/com/example/student2/samerandom/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.student2.samerandom;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    public static Sensor accel;
+    public  static SensorManager sm;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         MyDraw myDraw=(MyDraw)findViewById(R.id.myDraw1);
+        sm =(SensorManager) getSystemService(SENSOR_SERVICE);
+        accel=sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        textView=(TextView) findViewById(R.id.textView);
+        sm.registerListener(this,accel,SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
+    }
+
+    @Override
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+    }
+
+
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        textView.setText("v1:"+sensorEvent.values[0]+"\nv2:"+sensorEvent.values[1]+
+                             "\nv3:"+sensorEvent.values[2]);
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
--- app/src/main/java/com/example/student2/samerandom/Man.java
+++ app/src/main/java/com/example/student2/samerandom/Man.java
@@ -5,40 +5,83 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 
 /**
  * Created by student2 on 22.12.17.
  */
 
-public class Man {
+public class Man implements SensorEventListener{
 float x,y,r=5,vx=-5,vy=-5;
     Paint paint= new Paint();
     Bitmap sprites;
 
     int nFrame=0;
+    float turn=0;
+    private  float prevX;
+    float dx=15;
+    int spriteY=100;
 
     public  void  draw (Canvas canvas){
+        if (vx>0){
+            spriteY=100;
+        }
+        else {spriteY=300;}
         Rect to = new Rect((int) x-50,(int) y-50,(int) x+50,(int) y+50);
-        Rect frame =new Rect (nFrame*100,100,nFrame*100+100,200);
+        Rect frame =new Rect (nFrame*100,spriteY,nFrame*100+100,spriteY+100);
+        if (Math.abs(x-prevX)> dx){
         nFrame++;
         nFrame %=6;
+        prevX=x;}
 
 
         //paint.setColor(Color.BLACK);
         //canvas.drawCircle(x,y,r,paint);
         canvas.drawBitmap(sprites,frame,to,paint);
     }
+
     public Man (float x, float y,Bitmap sprites){
         this.x=x;
         this.y=y;
         this.sprites=sprites;
+        this.prevX=x;
+
     }
+    boolean connected;
     void move(){
+        if (!connected) {
+            MainActivity.sm.registerListener(this, MainActivity.accel, SensorManager.SENSOR_DELAY_NORMAL);
+            connected=true;
+        }
         if (x==10|| x==1000){
         this.vx=-this.vx;
+            turn++;
+            turn%=2;
+
         }
 
         this.x += this.vx;
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        final float threshold=2;
+        if (sensorEvent.values[0]>threshold){
+            vx=-5;
+
+        }
+        else if (sensorEvent.values[0]<-threshold){
+            vx=5;
+        }
+        else vx=0;
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
 
     }
 }
